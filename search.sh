d51131b3a68569edeb4b37a2130557d2c1fbbc1b
Tala Hethnawi <hethnawi.tala@gmail.com>
2021-11-09 20:11:42 +0200

    Add shell script for search endpoint

---
+++ search.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Read the user input
+echo "Enter the topic name: "
+read topic_name
+topic_name=$(python -c "import urllib; print urllib.quote('''$topic_name''')")
+response=$(curl -s "http://192.168.50.10:5000/search/${topic_name}")
+echo "The response is:"
+echo $response
